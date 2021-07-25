f40c001045b2265da52ba1129d1b31ff06c95d2a
Parada <paradafaii28@gmail.com>
2021-07-24 20:09:52 -0400

    finish video wigets, also debugging code

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,21 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
-  //going to run one time and run when term has been change
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (term) {
+        setDebouncedTerm(term);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -14,28 +26,45 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
+    search();
+  }, [debouncedTerm]);
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
+  //going to run one time and run when term has been change
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+  //       params: {
+  //         action: "query",
+  //         list: "search",
+  //         origin: "*",
+  //         format: "json",
+  //         srsearch: term,
+  //       },
+  //     });
+  //     setResults(data.query.search);
+  //   };
 
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 1000);
 
-  const renderdResults = results.map((result) => {
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+  // }, [term, results.length]);
+
+  const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
         <div className="right floated content">
@@ -66,7 +95,7 @@ const Search = () => {
           />
         </div>
       </div>
-      <div className="ui celled list">{renderdResults}</div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
